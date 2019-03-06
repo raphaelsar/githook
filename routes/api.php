d7d01138b69e4ae9c09fefe4f86c34379d49acb4
Raphael Saponeis <saponeis@gmail.com>
2019-03-06 16:55:39 -0300

    - Add api routes

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,10 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('api')->get('/users/{username}', function ($username) {
+    return $username;
+});
+
+Route::middleware('api')->get('/users/{username}/repos', function ($username) {
+    return $username;
 });
